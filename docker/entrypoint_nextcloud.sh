a4df96194337164b31ce90e252d721fa5fc6c779
CIMO <simone.dagostino@gmail.com>
2020-07-03 15:09:01 +0900

    Small fix.

--- docker/entrypoint_nextcloud.sh
+++ docker/entrypoint_nextcloud.sh
@@ -2,6 +2,7 @@
 
 tail -f /dev/null
 
+mv /home/setup-nextcloud.php /home${WWW_USER_NAME}/root${WEB_PATH}nextcloud/setup-nextcloud.php
 chmod 775 /home/${WWW_USER_NAME}/root${WEB_PATH}nextcloud
 chown ${USER_NAME}:${WEB_USER_GROUP} /home/${WWW_USER_NAME}/root${WEB_PATH}nextcloud
 
