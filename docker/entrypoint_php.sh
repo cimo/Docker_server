cafe64488add295ce6b90d7da8115ba58d2be483
CIMO <simone.dagostino@gmail.com>
2020-07-03 19:58:48 +0900

    Integrate in php container.

--- docker/entrypoint_php.sh
+++ docker/entrypoint_php.sh
@@ -3,6 +3,12 @@ set -e
 
 php-fpm -D
 
+# Nextcloud
+mkdir -p /home/${WWW_USER_NAME}/root${WEB_PATH}nextcloud
+cp /home/setup-nextcloud.php /home/${WWW_USER_NAME}/root${WEB_PATH}nextcloud/setup.php
+chmod 775 /home/${WWW_USER_NAME}/root${WEB_PATH}nextcloud
+chown cimo:www-data /home/${WWW_USER_NAME}/root${WEB_PATH}nextcloud
+
 tail -f /dev/null
 
 exec "$@"
